aa00f8bef2de3a1e667f573d5c59434dc6876047
garridopzk <nictehagarrido45@gmail.com>
2024-04-12 18:59:02 -0600

    ultimo commit

--- app/src/main/java/com/example/pizzeria/MainActivity3.java
+++ app/src/main/java/com/example/pizzeria/MainActivity3.java
@@ -2,8 +2,11 @@ package com.example.pizzeria;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,15 +17,106 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity3 extends AppCompatActivity {
     Button Menu;
     Button Refrescos;
+    EditText ingresar1;
+    EditText introducir;
+    EditText introducirtexto;
+    private int numPizzas = 0;
+    private int totalapagar = 0;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main3);
+
         Menu=findViewById(R.id.menu);
 
         Refrescos=findViewById(R.id.refrescos);
 
+        ingresar1=findViewById(R.id.ingresar1);
+
+        introducir=findViewById(R.id.ingresar2);
+
+        introducirtexto=findViewById(R.id.ingresar3);
+
+        numPizzas = 0;
+
+        totalapagar = 0;
+
+        EditText pizzaQueso = findViewById(R.id.ingresar1);
+        pizzaQueso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numPizzas++;
+                totalapagar += 150;
+
+                ingresar1.setText(String.valueOf(numPizzas));
+                ingresar1.setText(String.valueOf(totalapagar));
+            }
+        });
+
+        EditText PizzaHawallana = findViewById(R.id.ingresar2);
+        PizzaHawallana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numPizzas++;
+                totalapagar +=120;
+
+                introducir.setText(String.valueOf(numPizzas));
+                introducir.setText(String.valueOf(totalapagar));
+            }
+        });
+
+        EditText pizzapedepeperoni = findViewById(R.id.ingresar3);
+        pizzapedepeperoni.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numPizzas++;
+                totalapagar +=190;
+
+                introducirtexto.setText(String.valueOf(numPizzas));
+                introducirtexto.setText(String.valueOf(totalapagar));
+            }
+        });
+        ingresar1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int numpizzas, int totalapagar, int after) {
+
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try {
+                    numPizzas= Integer.parseInt(s.toString());
+                }catch (NumberFormatException e){
+                    numPizzas=0;
+                }
+
+            }
+
+             @Override
+             public void afterTextChanged(Editable s) {
+            }
+        });
+        introducir.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try {
+                    totalapagar= Integer.parseInt(s.toString());
+                }catch (NumberFormatException e){
+                    totalapagar=0;
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
         Menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/pizzeria/MainActivity4.java
+++ app/src/main/java/com/example/pizzeria/MainActivity4.java
@@ -1,7 +1,14 @@
 package com.example.pizzeria;
 
+import static android.view.View.*;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
@@ -11,15 +18,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity4 extends AppCompatActivity {
-    Button Anterior;
-    Button finalizarpedido;
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    Button Menu;
+    Button Finalizarpedido;
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main4);
 
+        Menu=findViewById(R.id.menu);
+
+        Finalizarpedido=findViewById(R.id.finalizarpedido);
+
+        Menu.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity4.this,MainActivity2.class);
+                startActivity(intent);
+
+            }
+        });
+        Finalizarpedido.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(MainActivity4.this, MainActivity5.class);
+                startActivity(intent);
+            }
+        });
     }
 }
-
